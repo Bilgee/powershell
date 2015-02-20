0e6e60d1f370c5598d22f377feec0c3cbf46c603
Bilgee Bayaraa <gd_bilgee@yahoo.com>
2015-02-20 17:14:02 +0900

    Update WordSearch.ps1
    Example: Search-Word -word TEST -path C:/documents

--- WordSearch.ps1
+++ WordSearch.ps1
@@ -1,11 +1,26 @@
-﻿$files = Get-ChildItem -Recurse -Path C:\scripts\Mod4 -File
-$temp_filename = ""
-foreach($file in $files){
-    (Get-Content $file.FullName) | foreach-object {if($_ -like "*param*"){
-        if($file.FullName -ne $temp_filename){
-            $file.FullName
-            $temp_filename = $file.FullName
-        }
-        $_
-    }} 
+function Search-Word
+{
+    Param(
+        [Parameter(Mandatory=$true)]
+        [String]$word,
+        [Parameter(Mandatory=$true)]
+        [String]$path
+    )
+
+    $files = Get-ChildItem -Recurse -Path $path -File
+    $temp_filename = ""
+    $pattern = "*"+$word+"*"
+    foreach($file in $files){
+        $line_number = 0;
+        (Get-Content $file.FullName) | foreach-object {
+            $line_number++
+            if($_ -like $pattern){
+                if($file.FullName -ne $temp_filename){
+                    $file.FullName
+                    $temp_filename = $file.FullName
+                }
+                echo "${line_number}:$_"
+            }
+        } 
+    }
 }

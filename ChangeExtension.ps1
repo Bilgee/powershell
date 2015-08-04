f5bbe1470de8083c5a94bf2402e0425f8bdec07c
Bilgee Bayaraa <gd_bilgee@yahoo.com>
2015-08-04 11:05:48 +0900

    Update and rename RenameFile.ps1 to ChangeExtension.ps1

---
+++ ChangeExtension.ps1
@@ -0,0 +1,18 @@
+function Change-Extension 
+{
+    Param(
+        [Parameter(Mandatory=$true)]
+        [String]$oldExtension,
+        [Parameter(Mandatory=$true)]
+        [String]$newExtension,
+        [Parameter(Mandatory=$true)]
+        [String]$path
+    )
+
+    $files = Get-ChildItem -path $path -recurse
+    foreach( $file in $files){
+        if($file.Extension -eq ".$oldExtension"){
+            Rename-Item $file.FullName  "$($file.DirectoryName)\$($file.Basename). $newExtension"
+        }
+    }
+}
--- RenameFile.ps1
+++
@@ -1,6 +0,0 @@
-﻿$files = Get-ChildItem -path "C:\Users\bbayarsaikhan\Desktop\temp\" -recurse
-foreach( $file in $files){
-    if($file.Extension -eq ".ps1"){
-        Rename-Item $file.FullName "$($file.FullName).txt"
-    }
-}
